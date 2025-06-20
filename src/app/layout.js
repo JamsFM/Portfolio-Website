2ba8fd1f28af2d4c02b6df651c765f074ea91270
JamsFM <jams_125@outlook.com>
2025-06-20 11:15:19 -0700

    feat: integrate Vercel Speed Insights in layout.js and remove from page.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import PropTypes from 'prop-types';
 // import Favicon from "react-favicon";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,30 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900 text-slate-50`}
       >
         {children}
+        <SpeedInsights
+          // url="https://jamesfmare.com"
+          // url="https://portfolio-website-jamesfmare.vercel.app/"
+          // url="http://localhost:3000/"
+          /* url="https://portfolio-website-jamesfmare.vercel.app/"
+          token={process.env.NEXT_PUBLIC_VERCEL_ANALYTICS_TOKEN}
+          className="fixed bottom-0 right-0 p-4 bg-gray-800/50 backdrop-blur-md rounded-lg shadow-lg z-50"
+          style={{ width: '300px', height: '200px' }}
+          options={{
+            lighthouse: {
+              performance: true,
+              accessibility: true,
+              bestPractices: true,
+              seo: true,
+              pwa: false,
+            },
+            insights: {
+              performance: true,
+              accessibility: true,
+              bestPractices: true,
+              seo: true,
+            },
+          }} */
+        />  
       </body>
     </html>
   );
